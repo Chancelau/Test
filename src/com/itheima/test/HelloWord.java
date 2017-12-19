2837304b8aa524778b319aa72a7a2eccdcb369be
LAPTOP-LIUYC\CC <chancelau@hotmail.com>
2017-12-19 10:10:47 +0800

    增加HelloWord类打印

---
+++ src/com/itheima/test/HelloWord.java
@@ -0,0 +1,10 @@
+package com.itheima.test;
+
+/**
+ * Created by CC on 2017/12/19.
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
